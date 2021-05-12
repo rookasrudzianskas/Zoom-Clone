7ea5cada48e8e1f2d461dea5007008f917211403
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 17:36:23 +0300

    Rokas

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 
+
 app.get('/', (req, res) => {
-    res.render('room');
+    // generate id, and redirect to it
+    res.redirect(`/${uuidv4()}`)
 })
+
+app.get('/:room', (req, res) => {
+    res.render('room', {roomId: req.params.room})
+})
+
 
 
 
