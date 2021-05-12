7d470c88b6c9499d8ce3b45f551cc1ba97c838b0
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 13:06:49 +0300

    Rokas

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const server = require('https').Server(app);
+
+app.get('/', (req, res) => {
+    res.status(200).
+})
+
+
+
+
+server.listen(3030);
