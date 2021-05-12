3001e356968f3c025ad62929a315321479927f3a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-12 17:47:16 +0300

    Rokas

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     // generate id, and redirect to it
+    // uuid generates the separate id
     res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:room', (req, res) => {
+    //passing the url from the link in the top
     res.render('room', {roomId: req.params.room})
 })
 
